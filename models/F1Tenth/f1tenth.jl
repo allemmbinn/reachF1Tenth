2dcafea2770f0d17dd7a7d1322702f772cc33a7a
Allen Emmanuel Binny <allemmbinn@gmail.com>
2023-12-18 21:00:45 +0530

    Add CORA

--- models/F1Tenth/f1tenth.jl
+++ models/F1Tenth/f1tenth.jl
@@ -1,27 +1,38 @@
 using ReachabilityAnalysis
 using ReachabilityAnalysis: TaylorModelReachSet, AbstractLazyReachSet
 
+vlim = 7.0;
+deltalim = 0.41;
+ratlim = deltalim/vlim;
+
+# Constraint for vx
 const ey1 = [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0] 
-const ey2 = [0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0] 
+# Constraint for vx
+const ey2 = [0.0, 0.0, 0.0, ratlim, 0.0, 0.0, 1.0]
+
 
 @taylorize function f1vehicle!(dx, x, p, t)
+    # x[9]  -> Frx
+    # x[10] -> dsteer
     lr = 0.17145
     lf = 0.15875
+    # m  = 3.47
 
     dx[1] = x[4] * cos(x[3]) - x[4] * sin(x[3])
     dx[2] = x[4] * sin(x[3]) + x[4] * cos(x[3])
     dx[3] = x[6]
-    dx[4] = one(x[1]) # Constant Accelartion
-    dx[5] = lr/(lr+lf) * (x[7])
-    dx[6] = 1 /(lr+lf) * (x[7])
-    dx[7] = zero(x[1]) # Constant Steering Angle
+    dx[4] = p[1]
+    dx[7] = p[2]
+    dx[5] = lr/(lr+lf) * (x[7]*dx[4] + dx[7]*x[4])
+    dx[6] = 1 /(lr+lf) * (x[7]*dx[4] + dx[7]*x[4])
     return dx
 end
 
 function f1vehicle()
     # X0 = (1.25 .. 1.55) × (2.35 .. 2.45) × (1.25 .. 1.55) × (2.35 .. 2.45) × b
-    X0 = Hyperrectangle([0.0, 0.0, 0.0, 0.0, 0., 0., 0.05],
-                        [0.3, 0.3, 0., 0.1, 0.1, 0., 0.])
+    X0 = Hyperrectangle([0.0, 0.0, 0.0, 1.0, 0.0, 0., 0.],
+                        [0.3, 0.3, 0.001, 0.1, 0.1, 0.001, 0.001])
+    U0 = ZeroSet(2);
     @ivp(x' = f1vehicle!(x), dim: 7, x(0) ∈ X0)
 end
 
--- models/F1Tenth/f1tenth_benchmark.jl
+++ models/F1Tenth/f1tenth_benchmark.jl
@@ -12,55 +12,55 @@ validation = []
 
 LazySets.deactivate_assertions()
 
-# ----------------------------------------
-# Case 1
-# ----------------------------------------
-println("Running Case 1: a = 1m/s^2; steering angle = 0.2 rad")
+
+println("Running Different discritisation of Input")
+
 prob = f1vehicle()
 alg = TMJets(;abstol=1e-7, orderT=5, orderQ=1, adaptive=false)
 
 # warm-up run
-sol = solve(prob, T=7.0, alg=alg)
+sol = solve(prob, T=3.0, alg=alg)
 
 # verify that specification holds
 solz = overapproximate(sol, Zonotope)
-
-property = (ρ(ey1, solz) < 4.95) && (ρ(ey2, solz) < 4.95)
+vlim = 7;
+deltalim = 0.41;
+property = (ρ(ey1, solz) < vlim) && (ρ(-1*ey1, solz) < vlim) && (ρ(ey2, solz) < deltalim) && (ρ(-1*ey2, solz) < deltalim)
 push!(validation, Int(property))
 
-# benchmark
-SUITE[model][cases[1]] = @benchmarkable solve($prob, T=10.0, alg=$alg)
+# # benchmark
+# SUITE[model][cases[1]] = @benchmarkable solve($prob, T=3.0, alg=$alg)
 
-# ==============================================================================
-# Execute benchmarks and save benchmark results
-# ==============================================================================
+# # ==============================================================================
+# # Execute benchmarks and save benchmark results
+# # ==============================================================================
 
-# tune parameters
-tune!(SUITE)
+# # tune parameters
+# tune!(SUITE)
 
-# run the benchmarks
-results = run(SUITE, verbose=true)
+# # run the benchmarks
+# results = run(SUITE, verbose=true)
 
-# return the sample with the smallest time value in each test
-println("minimum time for each benchmark:\n", minimum(results))
+# # # return the sample with the smallest time value in each test
+# # println("minimum time for each benchmark:\n", minimum(results))
 
-# return the median for each test
-println("median time for each benchmark:\n", median(results))
+# # # return the median for each test
+# # println("median time for each benchmark:\n", median(results))
 
-# export runtimes
-runtimes = Dict()
-for (i, c) in enumerate(cases)
-    t = median(results[model][c]).time * 1e-9
-    runtimes[c] = t
-end
+# # export runtimes
+# runtimes = Dict()
+# for (i, c) in enumerate(cases)
+#     t = median(results[model][c]).time * 1e-9
+#     runtimes[c] = t
+# end
 
-if !@isdefined io
-    io = stdout
-end
+# if !@isdefined io
+#     io = stdout
+# end
 
-for (i, c) in enumerate(cases)
-    print(io, "JuliaReach, $model, $c, $(validation[i]), $(runtimes[c])\n")
-end
+# for (i, c) in enumerate(cases)
+#     print(io, "JuliaReach, $model, $c, $(validation[i]), $(runtimes[c])\n")
+# end
 
 
 # ==============================================================================
@@ -72,8 +72,8 @@ plot!(fig, solz,  vars=(1, 2), lw=0.0, color=:blue,
     tickfont=font(5, "Times"), guidefontsize=5,
     xlab=L"x",
     ylab=L"y",
-    xtick= -2:0.5:15, ytick= -2:0.5:15,
-    xlims=(-2.5, 16), ylims=(-2.5, 16),
+    xtick= -1:5:100, ytick= -1:5:100,
+    xlims=(-2, 120), ylims=(-2, 120),
     bottom_margin=8mm, left_margin=2mm, right_margin=8mm, top_margin=3mm,
     size=(1000, 1000))
 hline!(fig, [3.7], lc=:red, ls=:dash, lw=2, lab="")
